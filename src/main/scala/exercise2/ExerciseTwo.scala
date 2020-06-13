796176d5b67f5cc7df883a88644dbda99506c315
Jostein Gogstad <jostein.gogstad@gmail.com>
2020-06-13 16:56:51 +0200

    Bonus part two, abstract over effect and data structure

--- src/main/scala/exercise2/ExerciseTwo.scala
+++ src/main/scala/exercise2/ExerciseTwo.scala
@@ -1,13 +1,13 @@
 package exercise2
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{Concurrent, ExitCase, ExitCode, IO, IOApp, Sync, Timer}
 
 import scala.util.Random
 import scala.concurrent.duration._
 import cats.data._
 import cats.implicits._
-import cats.effect.{ExitCase, IO, Timer}
 import cats.effect.concurrent.Deferred
+import cats.{Parallel, Traverse}
 
 object ExerciseTwo extends IOApp {
   case class Data(source: String, body: String)
@@ -23,7 +23,7 @@ object ExerciseTwo extends IOApp {
     proc.guaranteeCase {
       case ExitCase.Completed => IO { println(s"$name request finished") }
       case ExitCase.Canceled  => IO { println(s"$name request canceled") }
-      case ExitCase.Error(ex) => IO { println(s"$name errored") }
+      case ExitCase.Error(_) => IO { println(s"$name errored") }
     }
   }
 
@@ -31,21 +31,21 @@ object ExerciseTwo extends IOApp {
   case class CompositeException(ex: NonEmptyList[Throwable]) extends Exception("All race candidates have failed")
 
   // Implement this function:
-  def raceToSuccess[A](ios: NonEmptyList[IO[A]]): IO[A] = Deferred[IO, A].flatMap { deferred =>
-    val startAll = ios.parTraverse { io =>
-      io.flatMap(deferred.complete).attempt.start
+  def raceToSuccess[F[_]: Concurrent: Parallel, G[_]: Traverse, A](fs: G[F[A]]): F[A] = Deferred[F, A].flatMap { deferred =>
+    val startAll = fs.parTraverse { io =>
+      Concurrent[F].start(io.flatMap(deferred.complete).attempt)
     }
     startAll.flatMap { fibers =>
       val getResult = deferred.get.map(_.some)
       val allFailed = for {
         (errors, successes) <- fibers.traverse(_.join).map(_.toList.partitionEither(identity))
-        maybeA              <- if (successes.isEmpty) IO(println(errors.map(_.getMessage).mkString("\n"))).as(none[A]) else getResult
+        maybeA              <- if (successes.isEmpty) Sync[F].delay(println(errors.map(_.getMessage).mkString("\n"))).as(none[A]) else getResult
       } yield maybeA
 
-      IO.race(deferred.get, allFailed).flatMap {
+      Concurrent[F].race(deferred.get, allFailed).flatMap {
         case Left(a)        => fibers.parTraverse(_.cancel).as(a)
-        case Right(Some(a)) => IO.pure(a)
-        case Right(None)    => IO.raiseError(new Exception("Finished with error"))
+        case Right(Some(a)) => a.pure[F]
+        case Right(None)    => new Exception("Finished with error").raiseError[F, A]
       }
     }
   }
@@ -64,6 +64,6 @@ object ExerciseTwo extends IOApp {
       )
       .map(provider)
 
-    raceToSuccess(methods).as(ExitCode.Success)
+    raceToSuccess[IO, NonEmptyList, Data](methods).as(ExitCode.Success)
   }
 }
